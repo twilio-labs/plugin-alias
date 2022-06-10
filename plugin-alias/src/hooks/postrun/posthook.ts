c4a2a96b81bed25a765a658b3d17cdab59ce92e6
= <=>
2022-06-10 15:06:56 +0530

    Use runCommand() and initiate hooks

---
+++ plugin-alias/src/hooks/postrun/posthook.ts
@@ -0,0 +1,7 @@
+
+const posthook = async function (opts) {
+  //  console.log('Posthook ' + opts.id);
+
+}
+
+module.exports = posthook
---
+++ plugin-alias/src/hooks/prerun/prehook.ts
@@ -0,0 +1,5 @@
+const prehook = async function (opts) {
+  // console.log('Prehook ' + opts.id);
+}
+
+module.exports = prehook
